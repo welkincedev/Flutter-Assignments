02493080904f83b124b8b8350c71dab895513bb7
welkincedev <welkflutter@gmail.com>
2026-04-24 16:27:46 +0530

    changes in Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assginment/Assignments/Login%20Page,Register%20Page/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'Assignments/HotelBookUI.dart';
 import 'Assignments/listViewTask.dart';
@@ -5,5 +6,5 @@ import 'Assignments/stackCCard.dart';
 import 'Assignments/uitaskListGrid.dart';
 
 void main() => runApp(
-  MaterialApp(debugShowCheckedModeBanner: false, home: MonthListPage()),
+  MaterialApp(debugShowCheckedModeBanner: false, home: Loginpage()),
 );
